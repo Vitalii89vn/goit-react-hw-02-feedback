b976df535ac9ef0f3fdb226a8e6c347eb9caa515
Vitalii89vn <vitalii.kuhta@gmail.com>
2023-01-30 23:10:58 +0200

    t1

---
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -0,0 +1,17 @@
+import css from './Feedback.module.css';
+import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+    return (
+        <div>
+            {options.map((option, idx) => (
+                <button type="button" key={idx} className={css.btn_feedback} onClick={onLeaveFeedback}>{option[0].toUpperCase() + option.slice(1)}</button>
+            ))}
+           
+        </div>
+    )
+};
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+}
